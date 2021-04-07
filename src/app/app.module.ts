5878b52a6cc57be2dca12f23f094ddb1168d3c63
Faiber Juanias <faiberjuanias001@gmail.com>
2021-04-07 15:12:22 -0500

    Instalación de modulos de Firebase y configuración de Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from "angularfire2/database";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBRRuLY-XUPZ-8q_NriUPdXDtPmfmjtCqg',
+    authDomain: 'typescript-platzi-b5c2f.firebaseapp.com',
+    databaseURL: 'https://typescript-platzi-b5c2f-default-rtdb.firebaseio.com',
+    projectId: 'typescript-platzi-b5c2f',
+    storageBucket: 'typescript-platzi-b5c2f.appspot.com',
+    messagingSenderId: '1034783941205',
+    appId: '1:1034783941205:web:b7f94c670cdda3ba6baadc',
+  },
 };
 
 /*
