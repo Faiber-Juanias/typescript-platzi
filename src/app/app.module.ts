729f06ac699f0f3efd4f5be104292b83df110c9e
Faiber Juanias <faiberjuanias001@gmail.com>
2021-04-07 22:46:20 -0500

    Se crea el componente team-table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 
 import { environment } from 'src/environments/environment';
 import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from "angularfire2/database";
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { PlayerService } from './services/player.service';
 import { TeamService } from './services/team.service';
+import { TeamTableComponent } from './team-table/team-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamTableComponent
   ],
   imports: [
     BrowserModule,
