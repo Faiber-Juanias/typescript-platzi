41b64d91a5c5ba05ad7d5fe9abab812e4ee6da73
Faiber Juanias <faiberjuanias001@gmail.com>
2021-04-08 22:35:30 -0500

    Se crea el componente player-table y se muestra la tabla de players

---
+++ src/app/player-table/player-table.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Player } from '../interfaces/player';
+import { PlayerService } from '../services/player.service';
+
+@Component({
+  selector: 'app-player-table',
+  templateUrl: './player-table.component.html',
+  styleUrls: ['./player-table.component.scss']
+})
+export class PlayerTableComponent implements OnInit {
+
+  public players$: Observable<Player[]>;
+  public selectedPlayer: Player;
+  public showModal: boolean;
+
+  constructor(private playerService: PlayerService) { }
+
+  ngOnInit(): void {
+    this.players$ = this.playerService.getPlayers();
+  }
+
+  newPlayer() {
+    // Muestra el modal
+    this.showModal = true;
+    // Setea a null el player
+    this.selectedPlayer = null;
+    // Redirecciona
+    setTimeout(() => {
+      window.location.replace('#open');
+    });
+  }
+
+}
